5636f6f7978faee6ac88352d2a69a47f6297b050
Mandy <drgnfli6420@yahoo.com>
2020-03-19 19:13:51 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+alert("Hello, world")
+var firstName = "Mandy";
+console.log(firstName)
+var lastName = "Campbell"
+console.log(lastName)
+var age = 25
+console.log(age)
+var likestotravel = true
+console.log(likestotravel)
+var moviearray = ["Jurassic Park", "The Upside,"]
+console.log(moviearray[0])
+console.log(moviearray[1])
+
